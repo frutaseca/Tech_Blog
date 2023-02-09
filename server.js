111e3649b2067cf8de6804e860ecb693185a39c1
Brandon <brandonrodriguezvega@gmail.com>
2023-02-08 19:31:14 -0500

    Added code and ran test on it having some issuses still

--- server.js
+++ server.js
@@ -5,9 +5,29 @@ const exphbs = require("express-handlebars");
 const routers = require("./controllers");
 const sequelize = require("./config/connection")
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const sess = {
+    secret: "secert",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+    db: sequelize
+})
+};
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routers)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
